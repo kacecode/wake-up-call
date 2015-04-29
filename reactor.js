214b92746db40ba991d0c8c688124afc08834df1
sdarby <sdarby@izeni.com>
2015-04-29 17:58:47 -0600

    more error handeling

--- reactor.js
+++ reactor.js
@@ -35,7 +35,7 @@ var server = http.createServer(function (request, response) {
   request.on('end', function () {
     if (!body) {
       response.writeHead(400, {'Content-Type': 'text/plain'})
-      response.end('No body');
+      response.end('No body\n');
       return
     }
     var key = now();
@@ -45,7 +45,7 @@ var server = http.createServer(function (request, response) {
     } catch (e) {
       console.log(e);
       response.writeHead(400, {'Content-Type': 'text/plain'})
-      response.end("Bad JSON");
+      response.end("Bad JSON\n");
       return;
     }
     response.writeHead(201);
@@ -70,6 +70,12 @@ function finish (data, key) {
 }
 
 function createCb (key, json) {
+  if (!json) {
+    console.log("ERROR")
+    console.log("Created at ", key);
+    console.log("JSON", json)
+    return
+  }
   var data = JSON.parse(json);
   var time = calculateTime(new Date(data.datetime));
   var done = finish.bind(undefined, data, key);
